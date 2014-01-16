a066e18ebcb222f4e215ddf9c86985d933423fe6
John Brewer <jbrewer@jera.com>
2014-01-15 17:46:58 -0800

    Cleanup.

--- step1/step1.cpp
+++ step1/step1.cpp
@@ -1,20 +1,17 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-#define TRACKING_DURATION (1 * CLOCKS_PER_SEC)
-#define MIN_SIZE 150
-
 int main(int argc, char** argv) {
-	cv::VideoCapture cap(0);
+    cv::VideoCapture cap(0);
     if(!cap.isOpened())  // check if we succeeded
         return -1;
 
-	cv::Mat image;
+    cv::Mat image;
 
-	for (;;) {
-		cap >> image;
-		cv::imshow("image", image);
-	}
+    for (;;) {
+        cap >> image;
+        cv::imshow("image", image);
+    }
 
-	return 0;
+    return 0;
 }
